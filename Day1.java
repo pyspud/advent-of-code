be85918445c31913918fac3fc5785be1a8762f98
Kevin Henderson <pyspud@gmail.com>
2023-12-01 17:25:34 +0000

    Day1: part2

--- Day1.java
+++ Day1.java
@@ -20,9 +20,10 @@ import java.nio.file.Path;
 public class Day1 {
 
     public static void main(String... args) throws IOException {
-        var inputFile = Path.of("input1.txt");
+        var inputFile = Path.of("input2.txt");
         try (var lines = Files.lines(inputFile)) {
             var result = lines.map(Day1::calibrationDigits)
+                    // .peek(System.out::println)
                     .mapToInt(Digits::value)
                     .sum();
             System.out.println(result);
@@ -38,9 +39,11 @@ public class Day1 {
     }
 
     static Digits calibrationDigits(String line) {
-        char first =0;
-        char last =0;
-        for (var charElement : line.toCharArray()) {
+        char first = 0;
+        char last = 0;
+        for(var i=0; i<line.length();i++) {
+            var subLine = line.substring(i);
+            var charElement=numberAtStart(subLine);
             if (Character.isDigit(charElement)) {
                 if (first == 0) {
                     first = charElement;
@@ -49,5 +52,41 @@ public class Day1 {
             }
         }
         return new Digits(first, last);
+    }
+    
+    // two1nine
+    // eightwothree
+    // abcone2threexyz
+    // xtwone3four
+    // 4nineeightseven2
+    // zoneight234
+    // 7pqrstsixteen
+
+    static char numberAtStart(String line) {
+        var firstChar = line.charAt(0);
+        if (Character.isDigit(firstChar)) {
+            return firstChar;
+        } else if (line.startsWith("zero")) {
+            return '0';
+        } else if (line.startsWith("one")) {
+            return '1';
+        } else if (line.startsWith("two")) {
+            return '2';
+        } else if (line.startsWith("three")) {
+            return '3';
+        } else if (line.startsWith("four")) {
+            return '4';
+        } else if (line.startsWith("five")) {
+            return '5';
+        } else if (line.startsWith("six")) {
+            return '6';
+        } else if (line.startsWith("seven")) {
+            return '7';
+        } else if (line.startsWith("eight")) {
+            return '8';
+        } else if (line.startsWith("nine")) {
+            return '9';
+        }
+        return 0;
     }
 }
